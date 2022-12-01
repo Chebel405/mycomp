e88294d4263145a22e846c699242623ff984b8d0
Chebel405 <anne.chebel@gmail.com>
2022-12-01 12:50:14 +0100

    Package Personne

---
+++ src/main/java/fr/kira/formation/spring/competences/mycomp/personnes/PersonneServiceImpl.java
@@ -0,0 +1,37 @@
+package fr.kira.formation.spring.competences.mycomp.personnes;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PersonneServiceImpl implements PersonneService {
+
+    private final PersonneRepository personneRepository;
+
+    public PersonneServiceImpl(PersonneRepository personneRepository) {
+        this.personneRepository = personneRepository;
+    }
+
+    @Override
+    public List<Personne> findAll() {
+        return personneRepository.findAll();
+    }
+
+    @Override
+    public Personne save(Personne entity) {
+        return personneRepository.save(entity);
+    }
+
+    @Override
+    public Personne findById(String id) {
+        return personneRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public void deleteById(String id) {
+        personneRepository.deleteById(id);
+    }
+}
